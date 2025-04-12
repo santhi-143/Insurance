372b86927ca6982f8ab876676b39107c4e72ce69
santhi-143 <dsanthi.54@gmail.com>
2025-04-12 20:55:43 +0530

    Update Healthcare.java

--- Healthcare.java
+++ Healthcare.java
@@ -3,5 +3,7 @@ public class healthcare
     public static void main (String[] args)
     {
       system.out.println("Report")
+        system.out.println("Report files");
+      system.out.println("files");
         }
   }
